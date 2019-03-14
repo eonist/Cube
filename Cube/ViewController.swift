60f4ee969d8db94d2658f9b2ead722085883a213
Eonist <30n1st@gmail.com>
2019-03-14 20:22:02 +0100

    Add findVp3 functionality

--- Cube/ViewController.swift
+++ Cube/ViewController.swift
@@ -1,22 +1,26 @@
-
-
 import UIKit
 
+// TODO: add draw cirle, and draw line tools
+
 class ViewController: UIViewController {
+   //debug
+   lazy var vp1Dot:CAShapeLayer = createDot(color:.orange)
+   lazy var vp2Dot:CAShapeLayer = createDot(color:.blue)
+   lazy var quadCenterDot:CAShapeLayer = createDot(color:.yellow)
+   lazy var orthPTDot:CAShapeLayer = createDot(color:.red)
+   lazy var vp3Dot:CAShapeLayer = createDot(color:.purple)
+   //type
    typealias Line = (p1:CGPoint,p2:CGPoint)
    typealias Quad = (p1:CGPoint,p2:CGPoint,p3:CGPoint,p4:CGPoint)
    override func viewDidLoad() {
       super.viewDidLoad()
-     
-      
       //draw polyshape based on 4 points
       
       let polyShape:CAShapeLayer = .init()
       view.layer.addSublayer(polyShape)
-      let corners:[CGPoint] = [.init(x:100,y:130),.init(x:230,y:140),.init(x:200,y:220),.init(x:130,y:230)]
+      let corners:[CGPoint] = [.init(x:100,y:130),.init(x:220,y:150),.init(x:200,y:220),.init(x:130,y:240)]
       _ = CGShapeUtil.drawPolyLine(shapeLayer:polyShape, points:corners, style:(fillColor:nil,strokeColor:.black,thickness:1), close:true)
 
-      
       //get horizontal point pair / line pair
       let horA:Line = (corners[0],corners[1])
       let horB:Line = (corners[3],corners[2])
@@ -26,39 +30,67 @@ class ViewController: UIViewController {
       
       //find convering point of horizontal lines
       let vp1:CGPoint = Util.convergingPoint(a:horA,b:horB)
-      //find convering point of vertical lines
+//      Swift.print("vp1:  \(vp1)")
+      vp1Dot.frame.origin = vp1
+      
+      
+//      //find convering point of vertical lines
       let vp2:CGPoint = Util.convergingPoint(a:verA,b:verB)
-      
-      
-      //find diagonal center of quadereliteral
-      // note order: top left, top right, bottom left, bottom right
+      vp2Dot.frame.origin = vp2
+//
+//      //find diagonal center of quadereliteral
+//      // note order: top left, top right, bottom left, bottom right
       let quad:Quad = (corners[0],corners[1],corners[3],corners[2])
       let quadCenter:CGPoint = Util.diagonalCenter(quadPoints: quad)
-      //find line of 2 vanishing points
+      quadCenterDot.frame.origin = quadCenter
+      _ = createLine(line: (vp1,quadCenter), color: .gray)
+      _ = createLine(line: (vp2,quadCenter), color: .orange)
       
+//      //find line of 2 vanishing points
+//      Swift.print("vp1:  \(vp1)")
+//      Swift.print("vp2:  \(vp2)")
+      _ = createLine(line: (vp1,vp2), color: .purple)
       
-      
-      //find angel of ray from vp1 to vp3 (use vp2 ray and vp1 ray and substract 180)
+//      //find angel of ray from vp1 to vp3 (use vp2 ray and vp1 ray and substract 180)
       let QC_VP1_VP2:CGFloat = Trigo.angle(pivot:quadCenter,a:vp1,b:vp2)
-      let ORTHO_VP1:CGFloat = QC_VP1_VP2 - (CGFloat.pi / 4)
-      let VP1_QC_VP3:CGFloat = (CGFloat.pi / 4) - ORTHO_VP1
+      Swift.print("QC_VP1_VP2:  \(QC_VP1_VP2)")
+      Swift.print("CGFloat.pi:  \(CGFloat.pi)")
+//      let ORTHO_VP1:CGFloat = QC_VP1_VP2 - (CGFloat.pi / 4)
+//      Swift.print("ORTHO_VP1:  \(ORTHO_VP1)")
+      let span = abs(Trig.difference(Trig.angle(vp2, quadCenter), Trig.angle(quadCenter, vp1)))
+      Swift.print("span:  \(span)")
+      let VP1_QC_VP3:CGFloat = (CGFloat.pi / 2) - span
+      Swift.print(":  \(VP1_QC_VP3)")
       let VP1_QC:CGFloat = Trigo.angle(a: vp1, b: quadCenter)
-      let VP1_VP3:CGFloat = Trigo.normalize(angle:VP1_QC_VP3+VP1_QC)
-     
-      //find angel of ray from vp2 to vp3 🚫
-      
-      //find point on vanishingpointline, ortho to diagonal center of quad
+      Swift.print("VP1_QC:  \(VP1_QC)")
+      let VP1_VP3:CGFloat = Trigo.normalize(angle:VP1_QC+VP1_QC_VP3)
+      Swift.print("VP1_VP3:  \(VP1_VP3)")
+      let rayPT = vp1.add(CGPointParser.polar(300, VP1_VP3))
+      Swift.print("rayPT:  \(rayPT)")
+      _ = createLine(line: (vp1,rayPT), color: .green)
+////
+////      //find angel of ray from vp2 to vp3 🚫
+////
+////      //find point on vanishingpointline, ortho to diagonal center of quad
       let orthoPoint:CGPoint = Util.orthoPoint(line: (vp1,vp2), point: quadCenter)
-      
-      //find convering points of these two rays (this is vp3)
+      orthPTDot.frame.origin = orthoPoint
+      _ = createLine(line: (orthoPoint,quadCenter), color: .magenta)
+//
+////      //find convering points of these two rays (this is vp3)
       let ORTHO_PT_QUAD_PT:CGFloat = Trigo.angle(a: orthoPoint, b: quadCenter)
+      Swift.print("ORTHO_PT_QUAD_PT:  \(ORTHO_PT_QUAD_PT)")
       let vp3:CGPoint = Util.convergingPoint(a: vp1, aAngle: VP1_VP3, b: orthoPoint, bAngle: ORTHO_PT_QUAD_PT)
+      Swift.print("vp3:  \(vp3)")
+
+      vp3Dot.frame.origin = vp3
+      _ = createLine(line: (orthoPoint,vp3), color: .cyan)
       
-      //Draw vp-triangle
-      let vpTriangleShape:CAShapeLayer = .init()
-      view.layer.addSublayer(vpTriangleShape)
-      let vpPts:[CGPoint] = [vp1,vp2,vp3]
-      _ = CGShapeUtil.drawPolyLine(shapeLayer:vpTriangleShape, points:vpPts, style:(fillColor:nil,strokeColor:.blue,thickness:1), close:true)
+//
+//      //Draw vp-triangle
+//      let vpTriangleShape:CAShapeLayer = .init()
+//      view.layer.addSublayer(vpTriangleShape)
+//      let vpPts:[CGPoint] = [vp1,vp2,vp3]
+//      _ = CGShapeUtil.drawPolyLine(shapeLayer:vpTriangleShape, points:vpPts, style:(fillColor:nil,strokeColor:.blue,thickness:1), close:true)
       
       //use law of ratios to draw z-points of the cube.
       
@@ -73,12 +105,15 @@ extension ViewController{
       static func convergingPoint(a:Line,b:Line) -> CGPoint{
          let pointAndAngle:(p1:CGPoint,p2:CGPoint,a:CGFloat,b:CGFloat) = {
             let angleA:CGFloat = Trig.angle(a.p1, a.p2)
-            let angleB:CGFloat = Trig.angle(a.p1, a.p2)
+//            Swift.print("angleA:  \(angleA)")
+            let angleB:CGFloat = Trig.angle(b.p1, b.p2)
+//            Swift.print("angleB:  \(angleB)")
             let isConverging:Bool = CGPointAsserter.converging(a.p1, b.p1,angleA ,angleB )
+//            Swift.print("isConverging:  \(isConverging)")
             if isConverging {
                return (a.p1,b.p1,angleA,angleB)
             }else {
-               return (a.p2,b.p2,Trig.angle(a.p1, a.p2),Trig.angle(a.p1, a.p2))
+               return (a.p2,b.p2,Trig.angle(a.p1, a.p2),Trig.angle(b.p1, b.p2))
             }
          }()
          
@@ -88,7 +123,11 @@ extension ViewController{
        * diagonalCenter
        */
       static func diagonalCenter(quadPoints:Quad) -> CGPoint{
-         return .zero
+         let diagonalA:Line = (quadPoints.p1,quadPoints.p4)
+         let diagonalB:Line = (quadPoints.p2,quadPoints.p3)
+         //find convering point of horizontal lines
+         let vp1:CGPoint = Util.convergingPoint(a:diagonalA,b:diagonalB)
+         return vp1
       }
       /**
        * converingPoint (Ray
@@ -120,5 +159,23 @@ private class Trigo{
    }
    static func normalize(angle:CGFloat) -> CGFloat{
       return Trig.normalize(angle)
+   }
+}
+
+extension ViewController{
+   /**
+    *
+    */
+   func createDot(color:UIColor) -> CAShapeLayer{
+      let vp1Dot:CAShapeLayer = .init()
+      _ = CGShapeUtil.drawCircle(with: vp1Dot, circle: (.zero,4), style: (color,.clear,0), progress: 1)
+      view.layer.addSublayer(vp1Dot)
+      return vp1Dot
+   }
+   func createLine(line:Line,color:UIColor) -> CAShapeLayer{
+      let shape:CAShapeLayer = .init()
+      _ = CGShapeUtil.drawLine(lineLayer: shape, line: (line.p1,line.p2), style: (color,1))
+      view.layer.addSublayer(shape)
+      return shape
    }
 }
--- src/util/CGShapeUtil.swift
+++ src/util/CGShapeUtil.swift
@@ -1,4 +1,3 @@
-
 import UIKit
 
 class CGShapeUtil {
@@ -12,5 +11,41 @@ class CGShapeUtil {
       shapeLayer.lineWidth = style?.thickness ?? shapeLayer.lineWidth
       shapeLayer.fillColor = style?.fillColor?.cgColor
       return shapeLayer
+   }
+   /**
+    * Draw line
+    * NOTE: remember to: shapeLayer.addSublayer(lineLayer)
+    * It's also possible to do this with UIBezierPath
+    * let path:UIBezierPath = {
+    *    let aPath = UIBezierPath.init()//cgPath: linePath
+    *    aPath.move(to: p1)
+    *    aPath.addLine(to: p2)
+    *    aPath.close()//Keep using the method addLineToPoint until you get to the one where about to close the path
+    *    return aPath
+    * }()
+    */
+   static func drawLine( lineLayer:CAShapeLayer, line:(p1:CGPoint, p2:CGPoint), style:(stroke:UIColor,strokeWidth:CGFloat)) -> CAShapeLayer{
+      let path:CGMutablePath = CGMutablePath()
+      path.move(to: line.p1)
+      path.addLine(to: line.p2)
+      lineLayer.path = path/*Setup the CAShapeLayer with the path, colors, and line width*/
+      lineLayer.strokeColor = style.stroke.cgColor
+      lineLayer.lineWidth = style.strokeWidth
+      lineLayer.lineCap = .round
+      return lineLayer
+   }
+   /**
+    * Draws circle
+    * PARAM: progress: 0-1
+    */
+   static func drawCircle(with circleLayer:CAShapeLayer, circle:(center:CGPoint,radius:CGFloat), style:(fill:UIColor, stroke:UIColor, strokeWidth:CGFloat), progress:CGFloat) -> CAShapeLayer{
+      let circlePath = UIBezierPath(arcCenter: CGPoint(x: circle.center.x, y: circle.center.y), radius:circle.radius, startAngle: CGFloat(Double.pi * -0.5), endAngle: CGFloat(Double.pi * 1.5), clockwise: true)/*The path should be the entire circle, for the strokeEnd and strokeStart to work*/
+      circleLayer.path = circlePath.cgPath/*Setup the CAShapeLayer with the path, colors, and line width*/
+      circleLayer.fillColor = style.fill.cgColor
+      circleLayer.strokeColor = style.stroke.cgColor
+      circleLayer.lineWidth = style.strokeWidth
+      circleLayer.lineCap = .round
+      circleLayer.strokeEnd = progress/*Sets progress of the stroke between predefined start and predefined end*/
+      return circleLayer
    }
 }
